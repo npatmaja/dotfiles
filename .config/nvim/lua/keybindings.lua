00d8f3a377435fabb2f0bca00f723f5f486a0249
Nauval Atmaja <nauval.atmaja@gmail.com>
2022-06-12 21:42:25 +0700

    Add neovim config
    Because:
    Add neovim configuration based on https://tkg.codes/guide-to-modern-neovim-setup-2021/
    
    This commit:
    * Added working neovim config for mac

---
+++ .config/nvim/lua/keybindings.lua
@@ -0,0 +1,19 @@
+-- Remap space as leader key. Leader key is a special key that will allow us to
+-- make some additional keybindings. I'm using a comma, but you can use whatever
+-- you'd wish. We'll use it (for example) for searching and changing files
+-- (by pressing comma, then `s` and then `f`).
+vim.api.nvim_set_keymap('', '<Comma>', '<Nop>', { noremap = true, silent = true })
+vim.g.mapleader = ','
+vim.g.maplocalleader = ','
+
+-- Remap for dealing with word wrap. In general, the `nvim_set_keymap` is the
+-- function we can use to create any keymap you'd wish. I tend to keep those
+-- related to the whole editor here, while leaving the ones for specific plugins
+-- to their own configuration files. That way if I ever remove or change plugins
+-- I can keep my keybindings clean.
+vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
+
+-- Disable esc and replace it with `jk`
+vim.api.nvim_set_keymap('i', '<Escape>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', 'jk', '<Escape>', { noremap = true, silent = true })
+
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,49 @@
+-- Install vim-plug. 
+-- the `vim.fn.stdpath 'data'` yields `.local/share/nvim` 
+local install_path = vim.fn.stdpath 'data' .. '/site/autoload/plug.vim'
+local remote_vim_plug_url = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.execute('!curl -fLo ' .. install_path .. ' --create-dirs ' .. remote_vim_plug_url)
+end
+
+
+-- from https://dev.to/vonheikemen/neovim-using-vim-plug-in-lua-3oom
+local Plug = vim.fn['plug#']
+vim.call('plug#begin')
+
+Plug 'tpope/vim-fugitive'
+Plug 'tpope/vim-commentary'
+Plug 'tpope/vim-surround'
+Plug 'windwp/nvim-autopairs'
+Plug 'bluz71/vim-moonfly-colors'
+Plug 'itchyny/lightline.vim'
+Plug 'BurntSushi/ripgrep'
+Plug 'sharkdp/fd'
+Plug 'nvim-treesitter/nvim-treesitter'
+Plug 'nvim-treesitter/nvim-treesitter-textobjects'
+Plug 'neovim/nvim-lspconfig'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'saadparwaiz1/cmp_luasnip'
+Plug 'rafamadriz/friendly-snippets' -- Code snippets
+Plug 'L3MON4D3/LuaSnip' -- Autocompletion
+Plug 'nvim-lua/plenary.nvim'
+Plug 'lewis6991/gitsigns.nvim' -- Adds git-related info to the sign column, similar to gitgutter
+Plug 'nvim-telescope/telescope.nvim'
+Plug 'kyazdani42/nvim-web-devicons'
+Plug 'romgrk/barbar.nvim'
+
+-- run PlugInstall and PlugUpdate automatically, should be placed after the `plug#begin`
+-- as the plugin only be loaded after the call, error otherwise
+-- vim.api.nvim_command('PlugUpdate')
+-- vim.api.nvim_command('q')
+
+vim.call('plug#end')
+
+-- init
+require('gitsigns').setup()
+require('nvim-autopairs').setup{}
+require("luasnip.loaders.from_vscode").lazy_load()
+luasnip = require 'luasnip'
