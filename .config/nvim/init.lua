39297392ff99ebf7ce945b0b2f94713736c6ea9b
Your Name <your@email.com>
2022-06-13 22:56:15 +0700

    Make default colorscheme is dark
    Because:
    Apple specific condition to set the background to dark
    
    This commit:
    - updated init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -59,13 +59,6 @@ vim.api.nvim_exec(
   false
 )
 
--- Set dark theme if macOS theme is dark, light otherwise.
-local theme = vim.fn.system("defaults read -g AppleInterfaceStyle")
-if (string.find(theme, 'Dark')) then
-	vim.o.background = 'dark'
-	vim.cmd [[colorscheme moonfly]]
-else
-	vim.o.background = 'light'
-	vim.cmd [[colorscheme morning]]
-end
-
+-- Set dark theme
+vim.o.background = 'dark'
+vim.cmd [[colorscheme moonfly]]
