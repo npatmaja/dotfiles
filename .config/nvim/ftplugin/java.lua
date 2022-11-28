2f74177a86788d5042a22ccf520ddd805731e2e2
Your Name <your@email.com>
2022-11-29 00:26:00 +0700

    refactor(nvim): switch config to v2
    DEPRECATION NOTE: The old nvim config has renamed to `nvim@deprecated`
    and subject to remove soon

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -4,74 +4,77 @@ local home = os.getenv('HOME')
 local workspace_dir = home .. '/.local/share/eclipse/' .. project_name
 
 local java_plugin_path = {
-    ['jdtls'] = home .. '/.local/share/java/jdtls',
-    ['lombok'] = home .. '/.local/share/java/lombok',
-    ['java_debug'] = home .. '/.local/share/java/java-debug/com.microsoft.java.debug.plugin/target',
-    ['vscode_java_test'] = home .. '/.local/share/java/vscode-java-test/server',
+	['jdtls'] = home .. '/.local/share/java/jdtls',
+	['lombok'] = home .. '/.local/share/java/lombok',
+	['java_debug'] = home .. '/.local/share/java/java-debug/com.microsoft.java.debug.plugin/target',
+	['vscode_java_test'] = home .. '/.local/share/java/vscode-java-test/server',
 }
 
 local function getos()
-    local u_os = io.popen('uname -s', "r"):read('*l')
-    u_os = (u_os):lower()
+	local u_os = io.popen('uname -s', "r"):read('*l')
+	u_os = (u_os):lower()
 
-    local os_map = {
-        ['linux'] = 'linux',
-        ['darwin'] = 'mac'
-    }
+	local os_map = {
+		['linux'] = 'linux',
+		['darwin'] = 'mac'
+	}
 
-    local os_name = os_map[u_os]
-    if os_map[u_os] == nil then
-        os_name = os_map.linux
-    end
-    return os_name
+	local os_name = os_map[u_os]
+	if os_map[u_os] == nil then
+		os_name = os_map.linux
+	end
+	return os_name
 end
 
 local bundles = {
-    vim.fn.glob(java_plugin_path.java_debug .. "/com.microsoft.java.debug.plugin-*.jar")
+	vim.fn.glob(java_plugin_path.java_debug .. "/com.microsoft.java.debug.plugin-*.jar")
 }
 
 vim.list_extend(bundles, vim.split(vim.fn.glob(java_plugin_path.vscode_java_test .. '/*.jar'), "\n"))
 
 local config = {
-    cmd = {
-        'java',
-        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-        '-Dosgi.bundles.defaultStartLevel=4',
-        '-Declipse.product=org.eclipse.jdt.ls.core.product',
-        '-Dlog.protocol=true',
-        '-Dlog.level=ALL',
-        '-javaagent:' .. java_plugin_path.lombok .. '/lombok.jar',
-        '-Xms1g',
-        '--add-modules=ALL-SYSTEM',
-        '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-        '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-        '-jar', java_plugin_path.jdtls .. '/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
-        '-configuration', java_plugin_path.jdtls .. '/config_' .. getos(),
-        '-data', workspace_dir
-    },
-    root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
-    settings = {
-        java = {}
-    },
-    init_options = {
-        bundles = bundles
-    },
-    on_attach = function (client, bufnr)
-        
-        -- With `hotcodereplace = 'auto' the debug adapter will try to apply code changes
-        -- you make during a debug session immediately.
-        -- Remove the option if you do not want that.
-        jdtls.setup_dap({ hotcodereplace = 'auto' })
-        jdtls.setup.add_commands()
+	cmd = {
+		'java',
+		'-Declipse.application=org.eclipse.jdt.ls.core.id1',
+		'-Dosgi.bundles.defaultStartLevel=4',
+		'-Declipse.product=org.eclipse.jdt.ls.core.product',
+		'-Dlog.protocol=true',
+		'-Dlog.level=ALL',
+		'-javaagent:' .. java_plugin_path.lombok .. '/lombok.jar',
+		'-Xms1g',
+		'--add-modules=ALL-SYSTEM',
+		'--add-opens', 'java.base/java.util=ALL-UNNAMED',
+		'--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+		'-jar', java_plugin_path.jdtls .. '/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
+		'-configuration', java_plugin_path.jdtls .. '/config_' .. getos(),
+		'-data', workspace_dir
+	},
+	root_dir = require('jdtls.setup').find_root({ '.git', 'mvnw', 'gradlew' }),
+	settings = {
+		java = {}
+	},
+	init_options = {
+		bundles = bundles
+	},
+	on_attach = function(client, bufnr)
 
-        local opts = { silent = true, buffer = bufnr }
-        vim.keymap.set('n', "<A-o>", jdtls.organize_imports, opts)
-        vim.keymap.set('n', "<leader>df", jdtls.test_class, opts)
-        vim.keymap.set('n', "<leader>dn", jdtls.test_nearest_method, opts)
-        vim.keymap.set('n', "crv", jdtls.extract_variable, opts)
-        vim.keymap.set('v', 'crm', [[<ESC><CMD>lua require('jdtls').extract_method(true)<CR>]], opts)
-        vim.keymap.set('n', "crc", jdtls.extract_constant, opts)
+		-- With `hotcodereplace = 'auto' the debug adapter will try to apply code changes
+		-- you make during a debug session immediately.
+		-- Remove the option if you do not want that.
+		jdtls.setup_dap({ hotcodereplace = 'auto' })
+		jdtls.setup.add_commands()
 
+		local opts = { silent = true, buffer = bufnr }
+
+		vim.keymap.set('n', "<A-o>", jdtls.organize_imports, opts)
+		vim.keymap.set('n', "<leader>df", jdtls.test_class, opts)
+		vim.keymap.set('n', "<leader>dn", jdtls.test_nearest_method, opts)
+		vim.keymap.set('n', "crv", jdtls.extract_variable, opts)
+		vim.keymap.set('v', 'crm', [[<ESC><CMD>lua require('jdtls').extract_method(true)<CR>]], opts)
+		vim.keymap.set('n', "crc", jdtls.extract_constant, opts)
+
+		-- from lsp, if any changes in `/after/plugin/lsp.lua` changes, the following keymap should be updated
+		-- as well and vice versa
 		vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
 		vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
 		vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
@@ -93,17 +96,16 @@ local config = {
 			[[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
 		vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
 
-        local create_command = vim.api.nvim_buf_create_user_command
-        create_command(bufnr, 'W', require('me.lsp.ext').remove_unused_imports, {
-        nargs = 0,
-      })
-    end
+		local create_command = vim.api.nvim_buf_create_user_command
+
+		create_command(bufnr, 'W', require('me.lsp.ext').remove_unused_imports, { nargs = 0 })
+	end
 }
 
 require('jdtls').start_or_attach(config)
 
 -- set tab size to 4
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
-
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
---
+++ .config/nvim@deprecated/init.lua
@@ -0,0 +1,61 @@
+-- Dependencies
+require("plugins")
+require("keybindings")
+require("lsp")
+
+-- Plugins
+require("plug.treesitter")
+require("plug.cmp")
+require("plug.telescope")
+require("plug.nvimtree")
+require("plug.lspsaga")
+
+-- Set highlight on search. This will remove the highlight after searching for text.
+vim.o.hlsearch = false
+
+-- Make relative line numbers default. The current line number will be shown as well
+-- as relative numbering from that current line. It makes navigating around code easier.
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- Enable break indent.
+vim.o.breakindent = true
+
+-- Save undo history.
+vim.opt.undofile = true
+
+-- Case insensitive searching unless /C or capital in search.
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Decrease update time.
+vim.o.updatetime = 250
+vim.wo.signcolumn = 'yes'
+
+-- tab_size
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Set colorscheme defaults (order is important here).
+vim.o.termguicolors = true
+vim.g.onedark_terminal_italics = 2
+vim.o.background = 'dark'
+vim.cmd [[colorscheme moonfly]]
+
+-- Highlight on yank (copy). It will do a nice highlight blink of the thing you just copied.
+vim.api.nvim_exec(
+  [[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+  augroup end
+  set mouse+=a
+]],
+  false
+)
+
+-- Set dark theme
+vim.o.background = 'dark'
+vim.cmd [[colorscheme moonfly]]
