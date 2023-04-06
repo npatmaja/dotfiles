2bc5a3dca389bba32b12b6437576139ded1eeca3
Nauval Atmaja <nauval.atmaja@gmail.com>
2023-04-07 06:13:00 +0700

    refactor: remove deprecated nvim config

--- .config/nvim@deprecated/lua/plug/nvimtree.lua
+++
@@ -1,7 +0,0 @@
-require("nvim-tree").setup()
-
--- Keybindings
-vim.api.nvim_set_keymap('n', '<leader>tt', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<leader>tt', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>tf', ':NvimTreeFocus<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<leader>tf', ':NvimTreeFocus<CR>', { noremap = true, silent = true })
