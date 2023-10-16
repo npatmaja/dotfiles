2cddf9b36698f3b2efcdf0920c8e4e3cafa27218
Your Name <your@email.com>
2023-10-16 10:44:00 +0700

    chore(nvim): update telescope version

--- .config/nvim/lua/npatmaja/packer.lua
+++ .config/nvim/lua/npatmaja/packer.lua
@@ -18,7 +18,7 @@ return require('packer').startup(function(use)
 	-- telescope fuzzy finder
 	use({
 		'nvim-telescope/telescope.nvim',
-		tag = '0.1.0',
+		tag = '0.1.4',
 		requires = { { 'nvim-lua/plenary.nvim' } }
 	})
 	use('nvim-telescope/telescope-file-browser.nvim')
