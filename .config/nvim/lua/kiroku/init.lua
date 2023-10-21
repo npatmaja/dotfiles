d60a7e64aade3e954a222b9a53d925628d3b46cc
Nauval Atmaja <nauval.atmaja@gmail.com>
2023-10-21 22:21:46 +0700

    feat(kiroku): insert title from the selected link to markdown files
    before:
    [[some-file]]
    
    after _insert title_
    [[some-file]] Some Title

--- .config/nvim/lua/kiroku/init.lua
+++ .config/nvim/lua/kiroku/init.lua
@@ -25,6 +25,35 @@ tags:
 	}
 }
 
+local function get_title_frontmatter(filename)
+	local file = io.open(filename, "r")
+	if file == nil then
+		return ""
+	end
+	local title = ""
+	if file then
+		local frontmatter = true
+		local frontmatter_line_count = 0
+		while frontmatter do
+			local line = file:read("*l")
+			if line == nil then
+				break
+			end
+			if line == "---" then
+				frontmatter_line_count = frontmatter_line_count + 1
+			end
+			if frontmatter_line_count >= 2 then
+				frontmatter = false
+			end
+			if string.sub(line, 1, 6) == "title:" then
+				title = string.sub(line, 8, string.len(line))
+			end
+		end
+	end
+	file:close()
+	return title
+end
+
 function kiroku.format_cmp(entry, vim_item)
 	-- local kiroku_home = os.getenv("HOME") .. "/kiroku-copy"
 	local cwd = vim.fn.getcwd()
@@ -32,32 +61,10 @@ function kiroku.format_cmp(entry, vim_item)
 
 	if config.home == cwd and is_markdown and entry.completion_item.detail ~= nil then
 		-- get the front matter
-		local file = io.open(entry.completion_item.detail, "r")
-		if file == nil then
-			return vim_item
+		local title = get_title_frontmatter(entry.completion_item.detail)
+		if title:len() ~= 0 then
+			title = string.format("(%s)", title)
 		end
-		local title = ""
-		if file then
-			local frontmatter = true
-			local frontmatter_line_count = 0
-			while frontmatter do
-				local line = file:read("*l")
-				if line == nil then
-					break
-				end
-				if line == "---" then
-					frontmatter_line_count = frontmatter_line_count + 1
-				end
-				if frontmatter_line_count >= 2 then
-					frontmatter = false
-				end
-				if string.sub(line, 1, 6) == "title:" then
-					title = "(" .. string.sub(line, 8, string.len(line)) .. ")"
-				end
-			end
-		end
-		file:close()
-
 		local menu = ""
 		if vim_item.menu ~= nil then
 			menu = vim_item.menu
@@ -67,20 +74,10 @@ function kiroku.format_cmp(entry, vim_item)
 	return vim_item
 end
 
-function kiroku.newKiroku()
+function kiroku.new_kiroku()
 	local current_time = config.file.pattern()
 	local file_name = current_time .. config.file.extention
-	-- local file = io.open(file_name, "w")
-
-	-- file:write(config.template)
-	-- file:close()
-	-- Check if a tab is already open
-
-	-- local currentTab = vim.fn.tabpagenr()
-	-- local totalTabs = vim.fn.tabpagenr("$")
-	-- Check if the current buffer is empty
 	local isEmpty = vim.api.nvim_buf_line_count(0) <= 1 and vim.api.nvim_buf_get_lines(0, 0, -1, false)[1] == ""
-	-- print('currentTab: ' .. currentTab .. ' totalTabs: ' .. totalTabs)
 	if isEmpty == false then
 		vim.cmd('enew')
 	end
@@ -88,17 +85,54 @@ function kiroku.newKiroku()
 	vim.api.nvim_buf_set_name(0, file_name)
 	vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(config.template, "\n"))
 	-- Set the file type to Markdown
-	vim.api.nvim_buf_set_option(0, "filetype", config.file.type)
-	-- vim.cmd('tabnew')
-	-- vim.cmd('tabnext')
+	vim.api.nvim_set_option_value("filetype", config.file.type, { buf = 0 })
 
 	print("A new kiroku created: " .. file_name)
 end
 
+function kiroku.insert_title()
+	local vstart = vim.fn.getpos("'<")
+	local vend = vim.fn.getpos("'>")
+	local linestart = vstart[2]
+	local lineend = vend[2]
+	if linestart <= lineend then
+		linestart = linestart - 1
+	elseif linestart == lineend then
+		linestart = linestart - 1
+		lineend = linestart
+	end
+	local currbuf = vim.api.nvim_get_current_buf()
+	local lines = vim.api.nvim_buf_get_lines(currbuf, linestart, lineend, true)
+
+	for i, v in ipairs(lines) do
+		local extractedStrings = {}
+
+		for match in v:gmatch("%[%[(.-)%]%]") do
+			table.insert(extractedStrings, match)
+		end
+
+		local replacement_line = ""
+		for _, filename in ipairs(extractedStrings) do
+			local title = get_title_frontmatter(filename .. ".md")
+			local replacement_token = string.format("[[%s]] %s", filename, title)
+			local pattern = "%[%[" .. filename .. "%]%]"
+			replacement_line = v:gsub(pattern, replacement_token)
+		end
+		vim.api.nvim_buf_set_text(currbuf, linestart + i - 1, 0, linestart + i - 1, #v, { replacement_line })
+	end
+end
+
+-- keymapping
+vim.keymap.set("v", "<leader>ki", function() kiroku.insert_title() end, {})
+vim.keymap.set("n", "<leader>kn", function() kiroku.new_kiroku() end, {})
+
 -- create command
 vim.api.nvim_create_user_command('Kiroku', function(args)
 	if args.fargs[1] == 'new' then
-		kiroku.newKiroku()
+		kiroku.new_kiroku()
+	end
+	if args.fargs[1] == "insertTitle" then
+		kiroku.insert_title()
 	end
 end, {
 	range = true,
